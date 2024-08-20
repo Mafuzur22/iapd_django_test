5ce6cdee3b101f98b9210a7030bbc4c987f0c215
Mafuzur <mfrahaman22@gmail.com>
2024-08-20 21:30:17 +0600

    update3

--- test_project/test_app/models.py
+++ test_project/test_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class profileDataModel(models.Model):
+  name = models.CharField(max_length=50)
+  roll = models.PositiveIntegerField()
+  p_image = models.ImageField(upload_to='profile_images')
+  
+  def __self__(self):
+    return self.name
